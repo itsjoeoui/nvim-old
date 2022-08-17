1e42834e94d211c4ef57f06a1d45918fe3bfd372
Joey Yu <joey@itsjoeoui.com>
2022-08-17 17:20:21 -0400

    LSP!
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,9 @@
 vim.g.itsjoeoui_colorscheme = "tokyonight"
 
 function CureMyEyes()
-    vim.g.tokyonight_transparent_sidebar = true
+--    vim.g.tokyonight_transparent_sidebar = true
     vim.g.tokyonight_transparent = true
+    vim.g.tokyonight_dark_sidebar = true
 
     vim.cmd("colorscheme " .. vim.g.itsjoeoui_colorscheme)
 end
--- lua/itsjoeoui/packer.lua
+++ lua/itsjoeoui/packer.lua
@@ -14,5 +14,10 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-end)
+    use('neovim/nvim-lspconfig')
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
 
+    use("L3MON4D3/LuaSnip")
+end)
