1ce02a03ca0896b572048bd49f0013b52c2e69f1
Joey Yu <joey@itsjoeoui.com>
2022-08-20 13:06:27 -0400

    Lua lsp don't warn about vim global shit

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -111,7 +111,28 @@ require("lspconfig").gopls.setup {
     },
 }
 require('lspconfig').sumneko_lua.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = vim.split(package.path, ";"),
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
+		},
+	},
 }
 
 local tabnine = require('cmp_tabnine.config')
