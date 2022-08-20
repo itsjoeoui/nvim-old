1e971276c7dc8ef7032658be59532001a0f0f739
Joey Yu <joey@itsjoeoui.com>
2022-08-20 13:06:02 -0400

    More settings

--- lua/itsjoeoui/set.lua
+++ lua/itsjoeoui/set.lua
@@ -1,13 +1,16 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.errorbells = false
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.scrolloff = 3
+vim.opt.colorcolumn = '80'
+vim.opt.showmode = false
+vim.opt.showtabline = 2
